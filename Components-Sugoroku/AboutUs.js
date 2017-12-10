e18c1fd84c66706bd5f0d95c559111f316233b0b
nullP01nterException <jolina.lam@sbcglobal.net>
2017-12-10 00:38:21 -0800

    made website pretty and can show a simple board; need to add content

--- Components-Sugoroku/AboutUs.js
+++ Components-Sugoroku/AboutUs.js
@@ -5,21 +5,22 @@ class AboutUs extends React.Component{
 
 	render(){
 		return(
-			<div>
+			<div className="bodyText">
 				<h1>About Us</h1>
-				<p>December 2017
+				<div className="textBox">December 2017
 					We made this site to help people learn about this obscure and underappreciated genre of analog games, which is an ongoing research field at UCSC. The generator was created for Game AI class at UCSC.
 					This website is built with JavaScript and React.js, and the Board Generator is implemented using a variable-point crossover genetic algorithm in Python 3.6.
 					We are UCSC undergraduates studying Computer Science and Game Design! Hire Us!
-				</p>
-				<p>
-					Contact:
+				</div>
+
+				<h1>Contact Us</h1>
+				<div className="textBox">
 					Jon Alc
 					Yuvi Dube
 					Ryen Haimez
 					Joreeena Lam
 					Ryenuihong Yu
-				</p>
+				</div>
 			</div>
 		);
 	}
--- Components-Sugoroku/Navbar.js
+++ Components-Sugoroku/Navbar.js
@@ -6,9 +6,22 @@ class Navbar extends React.Component{
 	render(){
 		return(
 			<div id="navbar">
-				<IndexLink to="/" className="routerLink" activeClassName="active">Home</IndexLink>
-				<Link to="history" className="routerLink" activeClassName="active">History</Link>
-				<Link to="about" className="routerLink" activeClassName="active">About Us</Link>
+				<div id="navTitle">双六・Sugoroku</div>
+
+				<div id="navLinks">
+					<div className="navButton">
+						<IndexLink to="/" className="routerLink" activeClassName="active" className="link" id="home">Home</IndexLink>
+					</div>
+					<div className="navButton">
+						<Link to="history" className="routerLink" activeClassName="active" className="link" id="history">History</Link>
+					</div>
+					<div className="navButton">
+						<Link to="algorithm" className="routerLink" activeClassName="active" className="link" id="algorithm">Algorithm</Link>
+					</div>
+					<div className="navButton">
+						<Link to="about" className="routerLink" activeClassName="active" className="link" id="about">About Us</Link>
+					</div>
+				</div>
 			</div>
 		);
 	}
